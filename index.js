d20db26175782efc1197bdffed442bc12c2988c6
metaory <metaory@gmail.com>
2023-08-05 03:07:01 +0800

    reduced outcome

--- index.js
+++ index.js
@@ -1,21 +1,103 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+import { Octokit } from "@octokit/core";
+import {
+  paginateRest,
+  composePaginateRest,
+} from "@octokit/plugin-paginate-rest";
 
+import core from "@actions/core";
+import { readFile, writeFile } from "node:fs/promises";
 
-// most @actions toolkit packages have async methods
+const TARGET_TOPICS = [
+  "api",
+  "automation",
+  "challenge",
+  "cli",
+  "github-actions",
+  "npm-package",
+  "theme",
+];
+
+const auth = process.env["GH_PAT"];
+const username = "metaory";
+const listReposURL = "GET /users/metaory/repos";
+
+const [, , mode] = process.argv;
+const isDev = mode === "dev";
+
+const write = (data, path) => writeFile(path, JSON.stringify(data, null, 2));
+
+const read = async (path) =>
+  JSON.parse(await readFile(path, { encoding: "utf8" }));
+
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const octokit = new (Octokit.plugin(paginateRest))({ auth });
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    const res = isDev
+      ? await read("tmp/data.json")
+      : await octokit.paginate(listReposURL, { username });
 
-    core.setOutput('time', new Date().toTimeString());
+    isDev === false && (await write(res, "tmp/data.json"));
+
+    console.log("response:", res[0]);
+    console.log("length:", res.length);
+
+    const topicMap = TARGET_TOPICS.reduce(
+      (acc, cur) => ({ ...acc, [cur]: [] }),
+      {},
+    );
+
+    const reduced = res
+      .filter((x) => x.private === false && x.fork === false)
+      .reduce((acc, cur) => {
+        const {
+          name,
+          description,
+          topics,
+          updated_at,
+          stargazers_count,
+          language,
+        } = cur;
+
+        const { topic, match } = topics.reduce(
+          (_acc, _cur) => {
+            const topic = TARGET_TOPICS.find((x) => x === _cur);
+            if (topic && _acc.match === false) return { topic, match: true };
+            return _acc;
+          },
+          { topic: null, match: false },
+        );
+
+        if (match)
+          acc[topic].push({
+            name,
+            description,
+            updated_at,
+            stargazers_count,
+            language,
+          });
+
+        return acc;
+      }, topicMap);
+
+    console.log("-----------------");
+
+    const outcome = TARGET_TOPICS.reduce((acc, cur) => {
+      acc[cur] = reduced[cur].sort(
+        (a, b) => b.stargazers_count - a.stargazers_count,
+      );
+      return acc;
+    }, {});
+
+    console.log("outcome:", outcome);
+
+    write(outcome, "tmp/outcome.json");
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
 run();
+
+// ////////////////////////////////////////////////////////////////////////// //
+// const res = await octokit.request("GET /users/metaory/repos", { username });
